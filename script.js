88426acacd44a80b3d8a3e7763231e36b84ea217
Kiran K Channagiri <kirankc1704@gmail.com>
2024-01-22 15:19:42 +0530

    responsive nabar using js

---
+++ script.js
@@ -0,0 +1,7 @@
+let hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click",()=>
+{
+   let  nav = document.getElementById("nav");
+    nav.classList.toggle("dblock");
+});
+// Toggle between adding and removing a class name from an element with JavaScript.
